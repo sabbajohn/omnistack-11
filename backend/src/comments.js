0875da0151f52b381d8a85bb1d57d042c3bc68e9
sabbajohn <victorsabba@gmail.com>
2020-07-29 14:11:22 -0300

    Back-end da aplicação

---
+++ backend/src/comments.js
@@ -0,0 +1,26 @@
+/**
+    *  Rotas / Recurso
+*/
+/** Requisições HTTP
+    * GET
+    * POST
+    *  PUT
+    * DELETE
+ */
+ 
+ /**
+  * Tipos de Parametos
+  * Query params : Parametros nomeados enviado na rota apos ? (filtos e paginção)
+  * Route params : Usados para identificar recursos
+  * Request Body : Corpo da requisição para criar ou modificar dados 
+  *  */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL ...
+ * NoSQL: MongoDB, CounchDB etc 
+ */
+/**
+ * Drive : SELECT * FROM users
+ * Query Builder : table('users').select('*').where('') --> Universal !
+ */
+
