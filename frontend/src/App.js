3e8699454b989b1d05f980a30462ec172bc94cd5
sabbajohn <victorsabba@gmail.com>
2020-08-14 11:54:46 -0300

    estado e imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
+import Header from "./Header";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+    const [counter, setCounter] = useState(0);
+
+    function increment(params) {
+        setCounter(counter + 1);
+
+    }
+    return (
+        <div>
+            <Header>Contador:{counter}</Header>
+            <button onClick={increment} >Incrementar</button>
+        </div>
+    );
+
+
 }
 
 export default App;
